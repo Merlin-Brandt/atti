18f92f8d69e8ac6cc99dfabaf2a474b2a228f484
Merlin Brandt <merlin.brandt@hotmail.de>
2022-01-27 15:35:32 +0100

    Add Visual Studio project

---
+++ lib/sfml/build-linux-static/src/SFML/Window/CMakeFiles/sfml-window.dir/GlResource.cpp.o.d
@@ -0,0 +1,11 @@
+src/SFML/Window/CMakeFiles/sfml-window.dir/GlResource.cpp.o: \
+ /home/merlin/Projects/Programming/gfx/atti/lib/SFML-2.5.1/src/SFML/Window/GlResource.cpp \
+ /usr/include/stdc-predef.h \
+ /home/merlin/Projects/Programming/gfx/atti/lib/SFML-2.5.1/include/SFML/Window/GlResource.hpp \
+ /home/merlin/Projects/Programming/gfx/atti/lib/SFML-2.5.1/include/SFML/Window/Export.hpp \
+ /home/merlin/Projects/Programming/gfx/atti/lib/SFML-2.5.1/include/SFML/Config.hpp \
+ /home/merlin/Projects/Programming/gfx/atti/lib/SFML-2.5.1/include/SFML/System/NonCopyable.hpp \
+ /home/merlin/Projects/Programming/gfx/atti/lib/SFML-2.5.1/include/SFML/System/Export.hpp \
+ /home/merlin/Projects/Programming/gfx/atti/lib/SFML-2.5.1/src/SFML/Window/GlContext.hpp \
+ /home/merlin/Projects/Programming/gfx/atti/lib/SFML-2.5.1/include/SFML/Window/Context.hpp \
+ /home/merlin/Projects/Programming/gfx/atti/lib/SFML-2.5.1/include/SFML/Window/ContextSettings.hpp
